d0d307ae6020e82b30401bc987876c96c4742834
jiwootv <yooziwoo.coding0987@gmail.com>
2021-04-22 15:27:11 +0900

    eeeee

--- education/main.py
+++ education/main.py
@@ -1,20 +1,18 @@
 import time
-number1=0
-number2=0
-print ("계산기입니다. 숫자를 입력해 주세요.")
+print("계산기입니다. 숫자를 입력해 주세요.")
 time.sleep(1)
-number1=int(input('첫 번째 숫자는 무엇인가요?'))
+number1 = int(input('첫 번째 숫자는 무엇인가요?'))
 time.sleep(0.25)
-number2=int(input('두번째 숫자'))
+number2 = int(input('두번째 숫자'))
 time.sleep(1)
 print("ㅇㅋ")
 time.sleep(1)
-print('두 숫자의 합은', number1+number2, '입니다.' )
+print('두 숫자의 합은', number1+number2, '입니다.')
 time.sleep(0.25)
-print('두 숫자의 차는', number1+number2, '입니다.' )
+print('두 숫자의 차는', number1+number2, '입니다.')
 time.sleep(0.25)
-print('두 숫자의 나누기는', number1/number2, '입니다.' )
+print('두 숫자의 나누기는', number1/number2, '입니다.')
 time.sleep(0.25)
-print('두 숫자의 곱은', number1*number2, '입니다.' )
+print('두 숫자의 곱은', number1*number2, '입니다.')
 time.sleep(5)
 # ^ ^
