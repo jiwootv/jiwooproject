e789853bc31fc5b33473897155e2077ba933fdee
jiwootv <yooziwoo.coding0987@gmail.com>
2021-05-01 15:27:10 +0900

    와! pygame!

--- pygam.py
+++ pygam.py
@@ -1,11 +1,15 @@
 y = 100
 
 import pygame, sys
-sound = pygame.mixer.sound('')
-clock = pygame.time.Clock()
+
 pygame.init()
+
+sound = pygame.mixer.Sound('넌-모찌나간다-_방패병_.wav')
+clock = pygame.time.Clock()
+
 screen = pygame.display.set_mode((1300, 600))
 chracter_img = pygame.image.load('sans2 (1).svg')
+i_x, i_y = chracter_img.get_size()
 font = pygame.font.SysFont('malgungothic', 50)
 x = 0
 key = 0
@@ -18,12 +22,14 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == (pygame.K_a or pygame.K_RIGHT):
                 key = -1
+
             if event.key == (pygame.K_d or pygame.K_LEFT):
                 key = 1
             if event.key == (pygame.K_s or pygame.K_UP):
                 key_y = 1
             if event.key == (pygame.K_w or pygame.K_UP):
                 key_y = -1
+
         if event.type == pygame.KEYUP:
             key = 0
             key_y = 0
@@ -38,9 +44,17 @@ while True:
     a = font.render(f'x 좌표: {x} y 좌표:{y}', True, (255, 0, 0))
     screen.blit(chracter_img, (x, y))
     screen.blit(a, (650, 150))
-    if x > 1300:
+    if x > 1300 - i_x:
         x = 0
-    if -320 > x:
-        x = 1300
+        sound.play()
+    if 0 > x:
+        x = 1300 - i_x
+        sound.play()
+    if y > 600-i_y:
+        y = 0
+        sound.play()
+    if 0 > y:
+        y = 600 - i_y
+        sound.play()
     clock.tick(60)
     pygame.display.update()
