a91ff28a12994455e355cc2417932ddb8b3f06a2
jiwootv <yooziwoo.coding0987@gmail.com>
2021-08-01 10:36:06 +0900

    빠 빠 빠 빠 라 빠빠빠빠ㅏ빠빠빠빠ㅏㅃㅓㅏ빠ㅃ빠빠빠ㅏ빠ㅏ빠빠ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ빠빠빠ㅏ빠빠ㅏㅏ빠ㅏㅃ

---
+++ 숙제/PyGame/a.py
@@ -0,0 +1,16 @@
+import time
+
+time.sleep(1)
+
+import random
+
+random.random()
+random.randint(1, 10)
+
+# import pygame
+# pygame.init()
+
+import keyboard
+
+if keyboard.read_key() == 'z':
+    print('z키를 누르셨습니다.')
